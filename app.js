4cdf2e40880379e1c4246cc56b1131af8f2145d8
loustyvel <loustyveldidier@gmail.com>
2020-05-19 20:29:41 +0200

    go

---
+++ app.js
@@ -0,0 +1,49 @@
+const puppeteer = require('puppeteer');
+
+async function start_browser() {
+    browser = await puppeteer.launch({
+        // executablePath: 'C:/Program Files (x86)/Google/Chrome/Application/chrome.exe',
+        headless: true,
+        defaultViewport: null,
+        slowMo: 250,
+        // args: ['--window-size=800,860'],
+        args: ['--no-sandbox', '--start-maximized', '--disable-setuid-sandbox', '--disable-infobars', '--window-position=0,0', '--ignore-certifcate-errors', '--ignore-certifcate-errors-spki-list',]
+    })
+}
+
+
+start_browser().then(async () => {
+    const page = await browser.newPage();
+    await page.goto("https://www.linkedin.com/login?fromSignIn=true&trk=guest_homepage-basic_nav-header-signin/", { waitUntil: 'load' });
+    await page.waitFor(3000)
+    await page.click('#username')
+    await page.keyboard.type('loustyveldidier@gmail.com');
+    await page.click('[id="password"]')
+    await page.keyboard.type('@complexpro88');
+    await page.click('[type="submit"]')
+    await page.goto("https://www.linkedin.com/mynetwork/", { timeout: 0, waitUntil: 'load' });
+    await page.evaluate(() => {
+        Array.from(document.querySelectorAll('[data-control-name="people_connect"]'),
+            e => e.click())
+    });
+    await page.screenshot({ path: 'screenshot0.png' })
+
+    let connect = async function connect_() {
+
+        try {
+            await page.goto("https://www.linkedin.com/mynetwork/", { timeout: 0, waitUntil: 'load' });
+            await page.waitFor(10000)
+            await page.evaluate(() => {
+                Array.from(document.querySelectorAll('[data-control-name="people_connect"]'),
+                    e => e.click())
+            });
+            await page.screenshot({ path: 'screenshote01.png' })
+        } catch{
+            await page.screenshot({ path: 'screenshoterr.png' })
+        }
+    }
+    try { await setInterval(connect, 3600000); } catch{
+        await page.screenshot({ path: 'screenshoterr.png' })
+    }
+
+})
